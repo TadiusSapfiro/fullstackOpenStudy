1529c6df526746281287ebc1779099d971433c32
Volodymyr Zelentinov <v.zelentinov@gmail.com>
2026-01-12 11:37:37 +0200

    Refactor App component to remove Hello component and implement click handling logic

--- part1/part1-study-project/src/App.jsx
+++ part1/part1-study-project/src/App.jsx
@@ -1,21 +1,32 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </>
-  )
-}
+import {useState} from "react"
 
 const App = () => {
-  const name = "Katya"
-  const age = 23
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name = "Volodymyr" age = "28"/>
-      <Hello name = {name} age = {age + 6}/>
-    </>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+    </div>
   )
 }
 
--- part1/part1-study-project/src/main.jsx
+++ part1/part1-study-project/src/main.jsx
@@ -1,4 +1,5 @@
-import ReactDom from "react-dom/client"
-import App from "./App.jsx"
+import ReactDOM from 'react-dom/client'
 
-ReactDom.createRoot(document.getElementById("root")).render(<App />)
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
