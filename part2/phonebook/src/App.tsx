e7babe94e63bf210ec3460a7582c15f5c3524a44
Volodymyr Zelentinov <v.zelentinov@gmail.com>
2026-01-19 13:53:30 +0200

    feat: initialize phonebook application with React and TypeScript
    - Add package.json with dependencies and scripts for development
    - Include Vite logo SVG in public directory
    - Create main application component (App.tsx) with person form and list
    - Implement state management for adding and displaying names
    - Set up TypeScript configuration files for app and node
    - Configure Vite for React application
    
    2.6 done

---
+++ part2/phonebook/src/App.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+// import type { Person } from "./types";
+
+interface Person {
+	name: string;
+}
+type FormEvent = React.FormEvent<HTMLFormElement>;
+type InputEvent = React.ChangeEvent<HTMLInputElement>;
+interface PhonebookProps {
+	onAddPerson: (event: FormEvent) => void;
+	newName: string;
+	setNewName: React.Dispatch<React.SetStateAction<string>>;
+}
+const PersonForm = ({ onAddPerson, newName, setNewName }: PhonebookProps) => {
+	const handleNameChange = (event: InputEvent) => {
+		setNewName(event.target.value);
+	};
+
+	return (
+		<>
+			<h2>Phonebook</h2>
+			<form onSubmit={onAddPerson}>
+				<div>
+					Name: <input onChange={handleNameChange} value={newName} />
+				</div>
+				<div>
+					<button type="submit">Add new name</button>
+				</div>
+			</form>
+		</>
+	);
+};
+
+interface NumberProps {
+	person: Person;
+}
+const Number = ({ person }: NumberProps) => {
+	return <p>{person.name}</p>;
+};
+
+interface NumberListProps {
+	persons: Person[];
+}
+const NumbersList = ({ persons }: NumberListProps) => {
+	return (
+		<>
+			<h2>Numbers</h2>
+			<ul>
+				{persons.map((person) => (
+					<li key={person.name}>
+						<Number person={person} />
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
+
+const App = () => {
+	const [newName, setNewName] = useState("");
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const handleAddPerson = (event: FormEvent) => {
+		event.preventDefault();
+		setPersons(persons.concat({ name: newName }));
+		setNewName("");
+	};
+
+	return (
+		<>
+			<PersonForm
+				onAddPerson={handleAddPerson}
+				newName={newName}
+				setNewName={setNewName}
+			/>
+			<NumbersList persons={persons} />
+		</>
+	);
+};
+
+export default App;
