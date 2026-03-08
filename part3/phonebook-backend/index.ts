dda55b8b9e31a6ab0d5abd718cd684240ded0568
Volodymyr Zelentinov <v.zelentinov@gmail.com>
2026-03-08 16:30:36 +0200

    phonebook: update API base URLs to use production environment and improve port configuration

--- part3/phonebook-backend/index.ts
+++ part3/phonebook-backend/index.ts
@@ -99,7 +99,7 @@ const unknownEndpoint = (req: Request, res: Response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}`);
 });
