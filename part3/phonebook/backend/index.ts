e441c23d45c047825d7e303fce7c91dd83ccb25d
Volodymyr Zelentinov <v.zelentinov@gmail.com>
2026-03-18 13:54:54 +0200

    Improve MongoDB connection handling with error logging

--- part3/phonebook/backend/index.ts
+++ part3/phonebook/backend/index.ts
@@ -4,17 +4,27 @@ import morgan from "morgan";
 import type { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 
-const password = process.argv[2];
-
 const url = process.env.MONGODB_URI as string;
 
-mongoose.set("strictQuery", false);
+if (!url) {
+	console.error("CRITICAL ERROR: MONGODB_URI not found!");
+	process.exit(1);
+}
 
-mongoose.connect(url, { family: 4 });
+mongoose.set("strictQuery", false);
 interface IPerson {
 	name: string;
 	number: string;
 }
+
+mongoose
+	.connect(url, { family: 4 })
+	.then(() => {
+		console.log("Successful connection to MongoDB");
+	})
+	.catch((error) => {
+		console.error("Error connection to MongoDB:", error.message);
+	});
 
 const personSchema = new mongoose.Schema<IPerson>({
 	name: { type: String, required: true },
